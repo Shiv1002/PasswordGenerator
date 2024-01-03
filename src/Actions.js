6c4d40f2085af09b8cfb1cf16a5ec95ca90e0801
shivank <shivank.pasi@somaiya.edu>
2024-01-03 16:25:45 +0530

    update env

--- src/Actions.js
+++ src/Actions.js
@@ -1,10 +1,10 @@
 const tc_email = "shiv@gmail.com";
 const tc_pass = "kym mall";
-
+const DB_URL = import.meta.env.VITE_DB_URL || "http://localhost:1212";
 export const getPassword = async (email) => {
   return new Promise(async (resolve, reject) => {
     if (!email) reject("email undefined!");
-    await fetch("http://localhost:1212/get-password", {
+    await fetch(DB_URL + "/get-password", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
@@ -21,7 +21,7 @@ export const addPassword = async (email, password) => {
   return new Promise(async (resolve, reject) => {
     if (!email) reject("email undefined!");
     if (!password) reject("password undefined!");
-    await fetch("http://localhost:1212/add-password", {
+    await fetch(DB_URL + "/add-password", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
