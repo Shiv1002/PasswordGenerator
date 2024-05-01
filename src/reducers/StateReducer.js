3aad79d33c9eeae922a458df60fb74d1abe05390
shivank <shivank.pasi@somaiya.edu>
2024-05-01 18:32:17 +0530

    update

--- src/reducers/StateReducer.js
+++ src/reducers/StateReducer.js
@@ -8,7 +8,7 @@ export const initialSettings = {
 export const initialState = {
   profile: null,
   password: { pass: "", passwordFor: "", important: false },
-  history: JSON.parse(localStorage.getItem("pg-history")) || [],
+  history: [],
   settings: initialSettings,
 };
 
@@ -25,11 +25,9 @@ export function StateReducer(state, { type, payload }) {
       return { ...state, settings: { ...state.settings, [target]: value } };
     case "setHistory":
       // console.log(payload, state.history);
-      const n_history = [...payload, ...state.history];
-      console.log(n_history);
-      localStorage.setItem("pg-history", JSON.stringify(n_history));
-      console.log(localStorage.getItem("pg-history"));
-      return { ...state, history: [...payload, ...state.history] };
+      const set_history = [...payload, ...state.history];
+
+      return { ...state, history: set_history };
     case "logout":
       return { ...state, history: [], profile: initialState.profile };
     default:
